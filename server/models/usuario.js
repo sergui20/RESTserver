c03105009934e8421edf1989067c3ac1565ae45b
sergui20 <38196193+sergui20@users.noreply.github.com>
2019-03-11 14:45:07 -0500

    Update usuario.js

--- server/models/usuario.js
+++ server/models/usuario.js
@@ -1,14 +1,14 @@
-const mongoose = require('mongoose'); //To model our application DATA
-const uniqueValidator = require('mongoose-unique-validator'); //A plugin to validate unique fields and to handle some errors within the database
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
 
-let validRoles = { // Declarando roles validos para los usuarios
+let validRoles = {
 	values: ['ADMIN_ROLE', 'USER_ROLE'],
-	message: '{VALUE} no es un role valido' // {VALUE} es un parametro definido por mongoose que automaticamente nos manda el error
+	message: '{VALUE} no es un role valido'
 };
 
-let Schema = mongoose.Schema; //It is the configuration object for a Mongoose model
+let Schema = mongoose.Schema;
 
-let usuarioSchema = new Schema({ //Defining our model
+let usuarioSchema = new Schema({
 	nombre: {
 		type: String,
 		required: [true, 'El nombre es necesario']
@@ -41,14 +41,13 @@ let usuarioSchema = new Schema({ //Defining our model
 	}
 });
 
-usuarioSchema.methods.toJSON = function () { //Esto seria un Middleware segun yo. toJSON se ejecuta al covertir mis documentos a JSON format automaticamente, es por eso que es una funcion
-	let user = this; // this hace referencia al documento que lo obtenemos para eliminar ciertos parametros para mostrarlos al cliente
-	let userObject = user.toObject(); // Convertimos nuestro documento en un objeto para poder manipularlo
-	delete userObject.password; // Borramos la instancia password de nuestro objeto para que no aparezaca en la base de datos
-
+usuarioSchema.methods.toJSON = function () {
+	let user = this;
+	let userObject = user.toObject();
+	delete userObject.password;
 	return userObject;
 }
 
-usuarioSchema.plugin(uniqueValidator, {message: '{PATH} debe de ser unico'}); //uniqueValidator is a plugin which validates unique fields within a Mongoose schema
+usuarioSchema.plugin(uniqueValidator, {message: '{PATH} debe de ser unico'});
 
-module.exports = mongoose.model('Usuario', usuarioSchema); // Exporting our usuarioSchema with the name Usuario, both are clases with properties defined above
+module.exports = mongoose.model('Usuario', usuarioSchema);
